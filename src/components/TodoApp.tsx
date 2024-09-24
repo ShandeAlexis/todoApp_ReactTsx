16c856ad4a7e16234caf033597e832c5f2bde09d
Alexis Diaz <alexisshande@gmail.com>
2024-09-23 22:44:53 -0500

    Creacion del proyecto

---
+++ src/components/TodoApp.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export const TodoApp = () => {
+    const [nuevaTarea, setNuevaTarea] = useState('')
+  return (
+   <div>
+    <h1>Lista de tareas</h1>
+    <div>
+        <input 
+        type="text"
+        value={nuevaTarea}
+        onChange={(e)=>setNuevaTarea(e.target.value)}
+        placeholder="Nueva Tarea" />
+    </div>
+   </div>
+  )
+}
